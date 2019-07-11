6988b61c6cd47802c64cffa408bb83233d09d620
Jane <609490567@qq.com>
2019-07-11 14:14:45 +0800

    vue多页面开发，基于vue-cli2.9和3.0版本环境

---
+++ duoyemian2.9/vue-multipage/src/components/other.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <p @click="a">this is other page</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'other',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  methods:{
+    a(){
+      console.log(666)
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  div{
+    p{
+      font-size: 80px;
+      color:yellow;
+    }
+  }
+</style>
